d397622259867353e55b7b613c64d4a2ac412e37
brandon-glem <31105149+brandon-glem@users.noreply.github.com>
2026-06-13 21:26:09 -0500

    modificacion para conexion desde otra maquina

--- monopoly/project.clj
+++ monopoly/project.clj
@@ -3,12 +3,13 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-
+                 
   :dependencies [[org.clojure/clojure "1.12.2"]
                  [ring/ring-core "1.12.1"]
-                 [ring/ring-jetty-adapter "1.12.1"]
                  [ring/ring-json "0.5.1"]
-                 [compojure "1.7.1"]]
+                 [compojure "1.7.1"]
+                 [http-kit "2.8.0"]               
+                 [org.clojure/data.json "2.5.0"]] 
 
   :main ^:skip-aot monopoly.main
 
